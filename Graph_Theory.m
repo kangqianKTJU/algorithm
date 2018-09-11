ce5647f71c2cda0a89ca14cabc098525bdd146ed
kangqianKTJU <32949166+kangqianKTJU@users.noreply.github.com>
2018-09-11 21:30:01 +0800

    Add files via upload

--- Graph_Theory.m
+++ Graph_Theory.m
@@ -41,13 +41,14 @@ i = 2; % 记录路径的迭代次数
 while sum(short_index) < n
     [x, minIndex] = min(temp_distance); % 找到最短距离索引
     route(minIndex,i) = minIndex;   % 将最短索引写入路径矩阵
-    short_index(minIndex) = 1; 
+    short_index(minIndex) = 1;
+    unchanged_index = find(~short_index);
     temp_distance(minIndex) = inf;
     index = find(distance > distance(minIndex)+ a(minIndex,:)); % 记录下最小值改变的索引
     route(index,i)=minIndex;
     i=i+1;
     distance= min(distance,distance(minIndex)+ a(minIndex,:));
-    temp_distance = distance;  
+    temp_distance(unchanged_index) = distance(unchanged_index);  
 end
 distance
 route
@@ -82,6 +83,20 @@ ub = ones(1,m);
 result = intlinprog(c,intcon,[],[],Aeq,beq,lb,ub)
 
 %%
+a = zeros(11);
+a(1,2)=2;a(1,3)=8;a(1,4)=1;
+a(2,3)=6;a(2,5)=1;
+a(3,4)=7;a(3,5)=5;a(3,6)=1;a(3,7)=2;
+a(4,7)=9;
+a(5,6)=3;a(5,8)=2;a(5,9)=9;
+a(6,7)=4;a(6,9)=6;
+a(7,9)=3;a(7,10)=1;
+a(8,9)=7;a(8,11)=9;
+a(9,10)=1;a(9,11)=2;a(10,11)=4;
+a = a+ a';
+inf_element = find(~a);
+a(inf_element) = inf;   
+
 
  
 
