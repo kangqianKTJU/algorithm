585e71e9bbbc83a7b315a493880d9497cca95377
kangqianKTJU <32949166+kangqianKTJU@users.noreply.github.com>
2019-08-29 09:28:19 +0800

    列出目录下的所有文件及文件夹

--- Algorithm/src/InterviewPractice/FindFiles.java
+++ Algorithm/src/InterviewPractice/FindFiles.java
@@ -7,12 +7,36 @@ import java.util.List;
 public class FindFiles {
 
     public List<File> findFiles(String directory){
-        File dir = new File(directory);
         List<File> res = new ArrayList<>();
+        findFiles(directory, res, 0);
+        return res;
+    }
+    public List<File> findFiles(String directory, List<File> res,int depth){
+        File dir = new File(directory);
         File[] files = dir.listFiles();
         for(File fileOrDir:files){
-//            if()
+            if(fileOrDir.isFile()){
+                res.add(fileOrDir);
+                for (int i = 0; i <= depth; i++) {
+                    System.out.print("----");
+                }
+                System.out.println(fileOrDir);
+            }else{
+                for (int i = 0; i <= depth; i++) {
+                    System.out.print("----");
+                }
+                System.out.println(fileOrDir);
+                findFiles(fileOrDir.toString(), res, depth + 1);
+            }
         }
         return  null;
+    }
+
+    public static void main(String[] args) {
+        FindFiles solver = new FindFiles();
+        List<File> files = solver.findFiles("G:\\课程\\计算机组成原理");
+//        for(File file:files){
+//            System.out.println(file.toString());
+//        }
     }
 }
